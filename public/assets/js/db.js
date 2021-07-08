8fb00d6445b91b31de4cd3926ee47099a41bcce5
Stephen Yacas <syacas123@gmail.com>
2021-07-07 23:02:53 -0400

    modified db.js by adding checkDatabase function

--- public/assets/js/db.js
+++ public/assets/js/db.js
@@ -29,3 +29,36 @@ function saveRecord(record) {
     // add record to store
     store.add(record);
   }
+
+  function checkDatabase() {
+    // create a transaction on budgetStore db with readwrite access
+    const transaction = db.transaction(['budgetStore'], 'readwrite');
+    // access budgetStore object
+    const store = transaction.objectStore('budgetStore');
+    // get all records from store and set to a variable
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json',
+                },
+            })
+            .then(response => response.json())
+            .then(() => {
+                // if successful, open a transaction on budgetStore db
+                const transaction = db.transaction(['budgetStore'], 'readwrite');
+                // access budgetStore object
+                const store = transaction.objectStore('budgetStore');
+                // clear all items in store
+                store.clear();
+            });
+        }
+      };
+  }
+
+  window.addEventListener('online', checkDatabase);
