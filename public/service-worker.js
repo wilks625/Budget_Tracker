b2a4c9e4425f59976bbc8d9b7ae52a418a03c49a
Stephen Yacas <syacas123@gmail.com>
2021-07-07 23:10:52 -0400

    created variables for static and dynmaic cache as well as set up files to cache

---
+++ public/service-worker.js
@@ -0,0 +1,16 @@
+// Static cache (for static assets like css, js, html, images)
+const CACHE_NAME = "static-cache-v2";
+// dynamic cache for api routes
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_TO_CACHE = [
+    './',
+    './index.html',
+    './assets/css/style.css',
+    './assets/js/db.js',
+    './assets/js/index.js',
+    "./manifest.webmanifest",
+    './assets/images/icons/icon-192x192.jpg',
+    './assets/images/icons/icon-512x512.jpg',
+];
+
